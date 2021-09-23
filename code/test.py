fd75a57aff3c50473e979e35c1a5751349ac1024
mohsenbeygi <1383mohsenbeygi@gmail.com>
2021-09-23 21:23:11 +0200

    IB computer science

---
+++ code/test.py
@@ -0,0 +1,17 @@
+t = int(input())
+
+while t:
+    length = int(input())
+    line = input()
+    string = ''
+    for i in range(0, length):
+        if line[i] == 'D':
+            string += 'U'
+        elif line[i] == 'U':
+            string += 'D'
+        elif line[i] == 'L':
+            string += 'L'
+        elif line[i] == 'R':
+            string += 'R'
+    t-=1
+    print(string)
